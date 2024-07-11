3ce2f05e40148bb01f88f6f0a413adfb55d0b35e
No Brainer <nnnannn710@gmail.com>
2024-07-11 08:37:55 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
